c7b918e07b6e4bb66e2bba208210864ef4b161c1
p-sher4win <sher4winpereira@gmail.com>
2025-06-28 01:00:49 +0530

    first commit

---
+++ LeetCode 75/1071.py
@@ -0,0 +1,33 @@
+# Greatest Common Divisor of Strings
+
+str1 = input()
+str2 = input()
+
+quotient = []
+div = ''
+
+# Set N(Numerator) and D(denominator) based on size of each string 
+if len(str1) <= len(str2):
+    N, D = str2, str1
+else:
+    N, D = str1, str2
+
+# Using each pattern formed in div to remove the elements from each strings
+for i in range(len(D)):
+    # Create pattern
+    div += D[i]
+    
+    # Replace each pattern in the string with empty value
+    remainder1 = N.replace(div, '')
+    remainder2 = D.replace(div, '')
+
+    # If both strings are empty then, store the pattern formed to empty the strings
+    if remainder1 == '' and remainder2 == '':
+        quotient.append(div)
+
+# Print the longest pattern as the answer (last item in the list)
+if quotient:
+    print(quotient[-1])
+# Print empty string if no pattern
+else:
+    print('')
---
+++ LeetCode 75/1768.py
@@ -0,0 +1,24 @@
+# Merge Strings Alternatively
+
+word1 = input()
+word2 = input()
+merge = ''
+length = 0
+longer = ''
+
+# Check for lenght of each word to set the loop range to the lowest word size
+if len(word1) <= len(word2):
+    length = len(word1)
+    longer = word2
+else:
+    length = len(word2)
+    longer = word1
+
+# Loop through each index in range length and merge the value at that index
+for i in range(length):
+    merge += word1[i] + word2[i]
+
+# Adds the remaining values of the word that is longer than the other
+merge += longer[length:]
+
+print(merge)
