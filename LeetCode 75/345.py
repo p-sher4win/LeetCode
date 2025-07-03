64667019696589e0cd2f9ef83a5e5a977a890172
p-sher4win <sher4winpereira@gmail.com>
2025-07-04 01:03:13 +0530

    added 345.py

---
+++ LeetCode 75/345.py
@@ -0,0 +1,28 @@
+# Reverse Vowels of a String
+
+s  = "IceCreAm"
+
+vowels = "aeiouAEIOU"
+s = list(s)
+i, j = 0, len(s)-1
+
+# for i being < j
+while i<j:
+
+    # for value at an index not vowel
+    while i<j and s[i] not in vowels:
+        i+=1
+    while i<j and s[j] not in vowels:
+        j-=1
+
+    # swap values for corresponding index
+    temp = s[i]
+    s[i] = s[j]
+    s[j] = temp
+
+    # increment i and j
+    i+=1
+    j-=1
+
+# print the result list using join()
+print(''.join(s))
